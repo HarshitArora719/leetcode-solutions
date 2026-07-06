ea3636c74eeb16cc12d759aec3207ae21033afc3
Harshit Arora <harshitarora1972@gmail.com>
2026-07-06 11:45:23 +0530

    Time: 7 ms (52.31%), Space: 46.8 MB (11.27%) - LeetHub - Time: O(n log n), Space: O(1)

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] != b[0]) {
+                return Integer.compare(a[0], b[0]);
+            }
+            return Integer.compare(b[1], a[1]);
+        });
+
+        int count = 0;
+        int maxEnd = 0;
+        for (int[] interval : intervals) {
+            if (interval[1] > maxEnd) {
+                count++;
+                maxEnd = interval[1];
+            }
+        }
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
