98aa4c9fb793289372c47f57e79cdd7629099d3c
Harshit Arora <harshitarora1972@gmail.com>
2026-07-30 19:22:02 +0530

    Time: 0 ms (100%), Space: 43.1 MB (66.51%) - LeetHub - Time: O(1), Space: O(1)

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,9 +1,8 @@
 class Solution {
     public int minimumPushes(String word) {
         int n = word.length();
-        if(n < 9) return n;
         int a = n/8;
-        return 8 * a * (a+1)/2 + n%8 * (a+1);
+        return 4 * a * (a+1) + n%8 * (a+1);
     }
 }
 
