7db247e44b7e1c4911377bc4d945229ce8f66087
Harshit Arora <harshitarora1972@gmail.com>
2026-07-31 11:55:56 +0530

    Time: 9 ms (94.22%), Space: 48.3 MB (17.34%) - LeetHub - Time: O(n), Space: O(1)

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -11,7 +11,7 @@ class Solution {
 
         Arrays.sort(freq);
 
-        for(int i = 25; i >= 0; i--){
+        for(int i = 25; i >= 0  && freq[i] > 0; i--){
             ans += freq[i] * ( (25-i) / 8+1);
         }
 
